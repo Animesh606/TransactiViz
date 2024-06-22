c8269862212022cb8e47d5d41497a00ca043a5b1
Animesh <2025.cse18.animeshbag@gmail.com>
2024-06-22 22:37:41 +0530

    Changed configuration vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,38 +1,38 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//     plugins: [react()],
-//     build: {
-//         manifest: true,
-//         rollupOptions: {
-//             input: "./src/main.jsx",
-//         },
-//         outDir: "dist"
-//     },
-//     server: {
-//         proxy: {
-//             "/api": import.meta.env.VITE_API_KEY,
-//             cors: false
-//         },
-//     },
-// });
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
-
-    const API_URL = `${env.VITE_API_URL ?? "http://localhost:5000"}`;
-
-    return {
-        plugins: [react()],
-        server: {
-            proxy: {
-                "/api": API_URL,
-            },
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        // manifest: true,
+        // rollupOptions: {
+        //     input: "./src/main.jsx",
+        // },
+        outDir: "dist"
+    },
+    server: {
+        proxy: {
+            "/api": "https://transactiviz-api.onrender.com",
             cors: false
         },
-        build: {
-            outDir: "dist",
-        },
-    };
+    },
 });
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, process.cwd());
+
+//     const API_URL = `${env.VITE_API_URL ?? "http://localhost:5000"}`;
+
+//     return {
+//         plugins: [react()],
+//         server: {
+//             proxy: {
+//                 "/api": API_URL,
+//             },
+//             cors: false
+//         },
+//         build: {
+//             outDir: "dist",
+//         },
+//     };
+// });
