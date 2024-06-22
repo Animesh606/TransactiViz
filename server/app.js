a0fefa056d656aefe053f926108300db228fe8f2
Animesh <2025.cse18.animeshbag@gmail.com>
2024-06-22 23:54:46 +0530

    Add CORS to server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import transactionRouter from "./routers/transaction.router.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use("/api", transactionRouter);
 
 export default app;
